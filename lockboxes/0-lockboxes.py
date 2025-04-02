98f2309b068160105d2f6825aa7eb072a05a15d2
Diogo <diogoanonimos@gmail.com>
2025-04-02 13:36:46 +0200

    add function to determine if all boxes can be unlocked

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+def canUnlockAll(boxes):
+    """
+    Determines if all boxes can be unlocked.
+
+    Args:
+        boxes (list): A list of lists representing boxes and their keys.
+
+    Returns:
+        bool: True if all boxes can be unlocked, False otherwise.
+    """
+    unlocked = [0]
+    keys = []
+
+    while unlocked:
+        current_box = unlocked.pop()
+        for key in boxes[current_box]:
+            if key not in keys and key < len(boxes):
+                keys.append(key)
+                unlocked.append(key)
+
+    return len(keys) == len(boxes)
