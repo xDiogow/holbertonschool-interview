0625a377b9283a10039ad0724aa0a7984e3a414f
Diogo <diogoanonimos@gmail.com>
2025-04-14 17:39:09 +0200

    1-heap_insert.c

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+/**
+ * heap_insert - inserts a value into a Max Binary Heap
+ * @root: double pointer to the root node
+ * @value: value to insert
+ * Return: pointer to the inserted node, or NULL on failure
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
 	if (!root)
@@ -14,10 +20,12 @@ heap_t *heap_insert(heap_t **root, int value)
 	}
 
 	heap_t *parent = find_free_parent(*root);
+
 	if (parent == NULL)
 		return (NULL);
-	
+
 	heap_t *new_node = binary_tree_node(parent, value);
+
 	if (!new_node)
 		return (NULL);
 
@@ -29,11 +37,18 @@ heap_t *heap_insert(heap_t **root, int value)
 	return (heapify_up(new_node));
 }
 
+/**
+ * find_free_parent - Finds the first node that can take a new child
+ * @root: Pointer to the root of the binary heap
+ *
+ * Return: Pointer to the first node with at least one missing child,
+ *         or NULL if not found (which shouldn't happen in a valid heap).
+ */
 heap_t *find_free_parent(heap_t *root)
 {
 	heap_t *queue[1024];
 	int i = 0, size = 0;
-	
+
 	if (root == NULL)
 		return (NULL);
 
@@ -61,7 +76,7 @@ heap_t *find_free_parent(heap_t *root)
 heap_t *heapify_up(heap_t *node)
 {
 	int temp;
-	
+
 	while (node && node->parent && node->n > node->parent->n)
 	{
 		temp = node->n;
