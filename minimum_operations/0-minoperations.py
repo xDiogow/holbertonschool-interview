75a641e2af5711f23c59412e7cbe079943132243
Diogo <diogoanonimos@gmail.com>
2025-04-25 09:41:33 +0200

    0-m

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Min Operation Module"""
+
+def minOperations(n):
+    if n < 2:
+        return 0
+
+    total = 0
+    div = 2
+
+    while n > 1:
+        while n % div == 0:
+            total += div
+            n //= div
+        duv += 1
+
+    return total
