7268e8dbca4fb7d452e0b62ae4bafc0a9c84b2a3
Diogo <diogoanonimos@gmail.com>
2025-05-02 10:56:13 +0200

    sandpiles.h

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,7 +1,7 @@
-i#ifndef SANDPILES_H
+#ifndef SANDPILES_H
 #define SANDPILES_H
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void sandpiles_sum(int grid[3][3], int grid2[3][3]);
 void print_grid(int grid[3][3]);
 
 #endif /* SANDPILES_H */
