c3fd72d5ee0008f368a4646e79ca0ac56ac3ccce
Diogo <diogoanonimos@gmail.com>
2025-05-20 17:33:11 +0200

    0-check_cycle.c

---
+++ linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list, *fast = list;
+	
+	while (fast && fast->next)
+	{	
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return (1);
+	}
+
+	return (0);
+}
---
+++ linked_list_cycle/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gcc -Wall -Werror -Wextra -pedantic 0-main.c 0-check_cycle.c 0-linked_lists.c -o cycle
