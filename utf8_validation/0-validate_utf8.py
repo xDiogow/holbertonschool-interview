653d2720cff30b1117a9ab220f5d4c9e4039a6b8
Diogo <diogoanonimos@gmail.com>
2025-06-02 12:15:45 +0200

    0-validate_utf8.py

---
+++ utf8_validation/0-validate_utf8.py
@@ -0,0 +1,29 @@
+#!/bin/usr/python3
+"""
+Main file for UTF-8 Validation
+"""
+
+def validUTF8(data):
+    num_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF
+
+        if num_bytes == 0:
+            mask = 1 << 7
+            while mask & byte:
+                num_bytes += 1
+                mask >>= 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+
+        else:
+            if not (byte & 0b10000000 and not (byte & 0b01000000)):
+                return False
+        num_bytes -= 1
+
+        return num_bytes == 0
